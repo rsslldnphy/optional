a18b5a7065298990ecf783c9f4aea82f5d6b8eed
Russell <russell@russelldunphy.com>
2013-03-28 19:23:34 +0000

    Break out association spec into separate file

--- spec/lib/id_spec.rb
+++ spec/lib/id_spec.rb
@@ -74,28 +74,3 @@ describe Id::Model do
   end
 end
 
-describe Id::Model::HasOne do
-
-  module Foo
-    module Bar
-      module Baz
-        class Quux
-        end
-      end
-    end
-  end
-
-  let (:model) { stub(name: "Foo::Bar::Baz::Quux") }
-  let (:has_one) { Id::Model::HasOne.new(model, "yak", {}) }
-
-  describe "hierarchy" do
-    it "builds the class and module hierarchy for the model" do
-      has_one.hierarchy.constants.should eq [
-        Foo::Bar::Baz::Quux,
-        Foo::Bar::Baz,
-        Foo::Bar,
-        Foo
-      ]
-    end
-  end
-end
