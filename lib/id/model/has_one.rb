580bfc109bddd45959cb99ecf34daf98b8568a9d
Russell <russell@russelldunphy.com>
2013-04-03 12:30:37 +0100

    Default has_one associations to nil

--- lib/id/model/has_one.rb
+++ lib/id/model/has_one.rb
@@ -6,7 +6,7 @@ module Id
         field = self
         model.send :define_method, name do
           memoize field.name do
-            field.type.new(data.fetch(field.key) { raise MissingAttributeError })
+            field.type.new(data.fetch(field.key, nil))
           end
         end
       end
